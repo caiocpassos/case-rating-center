1975a5fdcd3b5cb81ca1ee6ad86127c0dceca132
Caio Passos <caio.passos@ifood.com.br>
2022-05-13 15:46:45 -0300

    Implements validation of addRate request

--- src/main/kotlin/com/example/demo/http/controller/RateController.kt
+++ src/main/kotlin/com/example/demo/http/controller/RateController.kt
@@ -1,6 +1,5 @@
 package com.example.demo.http.controller
 
-import com.example.demo.domain.entity.Rate
 import com.example.demo.domain.usecase.rating.AddRateUseCase
 import com.example.demo.domain.usecase.rating.GetAllRatesUseCase
 import com.example.demo.domain.usecase.rating.GetMerchantAverageUseCase
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/rates")
@@ -23,7 +23,7 @@ class RateController(
 ) {
 
     @PostMapping
-    fun addRate(@RequestBody rating: RateDto) =
+    fun addRate(@Valid @RequestBody rating: RateDto) =
         addRateUseCase.add(rateDtoToRatingConverter.convert(rating))
 
     @GetMapping
--- src/main/kotlin/com/example/demo/http/dto/RateDto.kt
+++ src/main/kotlin/com/example/demo/http/dto/RateDto.kt
@@ -1,8 +1,25 @@
 package com.example.demo.http.dto
 
+import javax.validation.constraints.DecimalMax
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
 data class RateDto(
+
+    @field:NotEmpty
+    @field:NotNull
     val orderId: String,
+
+    @field:NotEmpty
+    @field:NotNull
     val clientId: String,
+
+    @field:NotEmpty
+    @field:NotNull
     val merchantId: String,
+
+    @field:DecimalMax("5.0")
+    @field:DecimalMin("1.0")
     val rateValue: Double? = null
 )
